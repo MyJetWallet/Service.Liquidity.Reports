229e9f1d59f5e494d5dbd2a24bdb9f10161cfbe2
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-04-09 17:30:55 +0300

    test

--- test/Service.Liquidity.Reports.Tests/TestExample.cs
+++ test/Service.Liquidity.Reports.Tests/TestExample.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using NUnit.Framework;
 
 namespace Service.Liquidity.Reports.Tests
@@ -8,13 +9,33 @@ namespace Service.Liquidity.Reports.Tests
         [SetUp]
         public void Setup()
         {
+            
         }
 
         [Test]
         public void Test1()
         {
-            Console.WriteLine("Debug output");
-            Assert.Pass();
+            var a = 1.0;
+            var b = 0.1;
+            var c = 0.02;
+
+            var price = 100;
+
+            var dt = (a + b + c)*price;
+            var res = Math.Round((a + b + c)*price, 8);
+
+            var r1 = Math.Round(dt, 2, MidpointRounding.ToPositiveInfinity);
+
+            var r2 = Math.Round(Math.Round(dt, 4), 2, MidpointRounding.ToPositiveInfinity);
+
+
+            Console.WriteLine(dt);
+            Console.WriteLine(r1);
+            Console.WriteLine(r2);
+            Console.WriteLine(res);
+
+            Assert.AreNotEqual(112, dt);
+            Assert.AreEqual(112, res);
         }
     }
 }
