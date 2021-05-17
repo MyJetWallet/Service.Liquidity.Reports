b5f8bb1c2672119733d0768a159cc0396e1097eb
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-05-17 17:00:24 +0300

    update

--- src/Service.Liquidity.Reports/Modules/ServiceBusModule.cs
+++ src/Service.Liquidity.Reports/Modules/ServiceBusModule.cs
@@ -1,47 +1,23 @@
-﻿using System.Collections.Generic;
-using Autofac;
-using DotNetCoreDecorators;
-using Microsoft.Extensions.Logging;
+﻿using Autofac;
 using MyJetWallet.Sdk.Service;
+using MyJetWallet.Sdk.ServiceBus;
 using MyServiceBus.Abstractions;
-using MyServiceBus.TcpClient;
-using Service.Liquidity.Engine.Client.ServiceBus;
-using Service.Liquidity.Engine.Domain.Models.Portfolio;
+using Service.Liquidity.Engine.Client;
 
 namespace Service.Liquidity.Reports.Modules
 {
     public class ServiceBusModule : Module
     {
-        public static ILogger ServiceBusLogger { get; set; }
-
         protected override void Load(ContainerBuilder builder)
         {
-            ServiceBusLogger = Program.LogFactory.CreateLogger(nameof(MyServiceBusTcpClient));
-
-            var serviceBusClient = new MyServiceBusTcpClient(Program.ReloadedSettings(e => e.SpotServiceBusHostPort), ApplicationEnvironment.HostName);
-            serviceBusClient.Log.AddLogException(ex => ServiceBusLogger.LogInformation(ex, "Exception in MyServiceBusTcpClient"));
-            serviceBusClient.Log.AddLogInfo(info => ServiceBusLogger.LogDebug($"MyServiceBusTcpClient[info]: {info}"));
-            serviceBusClient.SocketLogs.AddLogInfo((context, msg) => ServiceBusLogger.LogInformation($"MyServiceBusTcpClient[Socket {context?.Id}|{context?.ContextName}|{context?.Inited}][Info] {msg}"));
-            serviceBusClient.SocketLogs.AddLogException((context, exception) => ServiceBusLogger.LogInformation(exception, $"MyServiceBusTcpClient[Socket {context?.Id}|{context?.ContextName}|{context?.Inited}][Exception] {exception.Message}"));
-
-            builder.RegisterInstance(serviceBusClient).AsSelf().SingleInstance();
+            var serviceBusClient = builder.RegisterMyServiceBusTcpClient(Program.ReloadedSettings(e => e.SpotServiceBusHostPort), ApplicationEnvironment.HostName, Program.LogFactory);
 
             var queryName = "Liquidity-Reports";
 
             builder
-                .RegisterInstance(new PortfolioTradeSubscriber(serviceBusClient, queryName, TopicQueueType.Permanent))
-                .As<ISubscriber<IReadOnlyList<PortfolioTrade>>>()
-                .SingleInstance();
-
-            builder
-                .RegisterInstance(new PositionAssociationSubscriber(serviceBusClient, queryName, TopicQueueType.Permanent))
-                .As<ISubscriber<IReadOnlyList<PositionAssociation>>>()
-                .SingleInstance();
-
-            builder
-                .RegisterInstance(new PositionPortfolioSubscriber(serviceBusClient, queryName, TopicQueueType.Permanent))
-                .As<ISubscriber<IReadOnlyList<PositionPortfolio>>>()
-                .SingleInstance();
+                .RegisterPortfolioTradeSubscriber(serviceBusClient, queryName, TopicQueueType.Permanent)
+                .RegisterPositionPortfolioSubscriber(serviceBusClient, queryName, TopicQueueType.Permanent)
+                .RegisterPositionAssociationSubscriber(serviceBusClient, queryName, TopicQueueType.Permanent);
         }
     }
 }
