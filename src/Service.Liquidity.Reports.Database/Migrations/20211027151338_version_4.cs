7760001c235aa69fa9b791683b27ab739077ec36
Pavlo Pylypenko <85163968+pylypenko-smplt@users.noreply.github.com>
2021-10-27 18:14:09 +0300

    updated feeshare table

---
+++ src/Service.Liquidity.Reports.Database/Migrations/20211027151338_version_4.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Service.Liquidity.Reports.Database.Migrations
+{
+    public partial class version_4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OperationId",
+                schema: "lp_reports",
+                table: "feesharesettlementhistory",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OperationId",
+                schema: "lp_reports",
+                table: "feesharesettlementhistory");
+        }
+    }
+}
