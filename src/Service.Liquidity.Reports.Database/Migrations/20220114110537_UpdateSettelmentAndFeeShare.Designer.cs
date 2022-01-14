e4fbc879c480c8e69eef07da42a41ef118dd223e
Yuriy Plyaskin <yuriy.p@smplt.net>
2022-01-14 14:32:17 +0300

    Update migration

--- src/Service.Liquidity.Reports.Database/Migrations/20220114110537_UpdateSettelmentAndFeeShare.Designer.cs
+++ src/Service.Liquidity.Reports.Database/Migrations/20220114110537_UpdateSettelmentAndFeeShare.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Service.Liquidity.Reports.Database;
@@ -11,9 +12,10 @@ using Service.Liquidity.Reports.Database;
 namespace Service.Liquidity.Reports.Database.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20220114110537_UpdateSettelmentAndFeeShare")]
+    partial class UpdateSettelmentAndFeeShare
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Service.Liquidity.Reports.Database/Migrations/20220114110537_UpdateSettelmentAndFeeShare.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Service.Liquidity.Reports.Database.Migrations
+{
+    public partial class UpdateSettelmentAndFeeShare : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
