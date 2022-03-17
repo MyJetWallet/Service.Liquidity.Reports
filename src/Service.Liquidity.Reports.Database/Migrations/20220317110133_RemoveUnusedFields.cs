4c1b60a715c4b55519629c53a1d9460164b5b2a9
Yuriy Plyaskin <yuriy.p@smplt.net>
2022-03-17 14:13:53 +0300

    Fix Db

---
+++ src/Service.Liquidity.Reports.Database/Migrations/20220317110133_RemoveUnusedFields.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Service.Liquidity.Reports.Database.Migrations
+{
+    public partial class RemoveUnusedFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReleasedPnl",
+                schema: "lp_reports",
+                table: "feesharesettlementhistory");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "ReleasedPnl",
+                schema: "lp_reports",
+                table: "feesharesettlementhistory",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
