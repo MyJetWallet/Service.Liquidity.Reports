46a4e781d65ffe813a188991dbce8fd7e6660218
Maxim Gorin <mxmgorin@gmail.com>
2022-03-17 19:34:01 +0200

    Change fee volume data type

---
+++ src/Service.Liquidity.Reports.Database/Migrations/20220317173130_change_fee_volume_type.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Service.Liquidity.Reports.Database.Migrations
+{
+    public partial class change_fee_volume_type : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "FeeVolume",
+                schema: "lp_reports",
+                table: "HedgeTrades",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FeeVolume",
+                schema: "lp_reports",
+                table: "HedgeTrades",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+    }
+}
