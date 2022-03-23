38cb1b2a7c5b20097506122d69cc0cfe328659d2
Maxim Gorin <mxmgorin@gmail.com>
2022-03-23 17:28:14 +0200

    Add order type to hedge trade

--- src/Service.Liquidity.Reports.Database/Migrations/20220323152728_Add_order_type_to_hedge_trade.Designer.cs
+++ src/Service.Liquidity.Reports.Database/Migrations/20220323152728_Add_order_type_to_hedge_trade.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Service.Liquidity.Reports.Database;
@@ -11,9 +12,10 @@ using Service.Liquidity.Reports.Database;
 namespace Service.Liquidity.Reports.Database.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20220323152728_Add_order_type_to_hedge_trade")]
+    partial class Add_order_type_to_hedge_trade
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
