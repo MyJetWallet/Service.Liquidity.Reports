0c59829da015b396e823d7478fe5bb244b47b4fd
Maxim Gorin <maksim.g@smplt.net>
2022-04-26 19:27:42 +0300

    Add side to hedge operation

---
+++ src/Service.Liquidity.Reports.Database/Migrations/20220426162638_add_side_to_hedge_operation.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Service.Liquidity.Reports.Database.Migrations
+{
+    public partial class add_side_to_hedge_operation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TargetSide",
+                schema: "lp_reports",
+                table: "HedgeOperations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TargetSide",
+                schema: "lp_reports",
+                table: "HedgeOperations");
+        }
+    }
+}
