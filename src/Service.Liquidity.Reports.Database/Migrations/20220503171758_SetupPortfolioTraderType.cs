0515bdc83122db56989b35d568e4680bb1c73dd0
Yuriy Plyaskin <yuriy.p@smplt.net>
2022-05-03 20:39:07 +0300

    Update

---
+++ src/Service.Liquidity.Reports.Database/Migrations/20220503171758_SetupPortfolioTraderType.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Service.Liquidity.Reports.Database.Migrations
+{
+    public partial class SetupPortfolioTraderType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE lp_reports.assetportfoliotrades SET \"Type\"=1 WHERE \"Comment\" LIKE 'Swap%';");
+            migrationBuilder.Sql("UPDATE lp_reports.assetportfoliotrades SET \"Type\"=2 WHERE \"Source\" LIKE '%anual';");
+            migrationBuilder.Sql("UPDATE lp_reports.assetportfoliotrades SET \"Type\"=3 WHERE \"Source\" LIKE 'Hedger';");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
